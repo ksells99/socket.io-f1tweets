af6015e4e75f4e0b3570d6a5b715aface5f3e9db
Kai Sells <kai.sells@hotmail.co.uk>
2021-01-03 16:56:20 +0000

    Setup & dependencies, initial Twitter API routes

---
+++ server/index.js
@@ -0,0 +1,7 @@
+const needle = require("needle");
+const config = require("dotenv").config();
+const TOKEN = process.env.TWITTER_BEARER_TOKEN;
+
+const rulesURL = "http://api.twitter.com//2/tweets/search/stream/rules";
+const streamURL =
+  "http://api.twitter.com//2/tweets/search/stream?tweet.field=public_metrics&expansions=author_id";
