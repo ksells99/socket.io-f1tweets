2759da18bfb436a69b8d7c8dbf0659ab02890b8f
Kai Sells <kai.sells@hotmail.co.uk>
2021-01-04 18:50:56 +0000

    HTML & styling added for received tweets

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ const rulesURL = "https://api.twitter.com/2/tweets/search/stream/rules";
 const streamURL =
   "https://api.twitter.com/2/tweets/search/stream?tweet.fields=public_metrics&expansions=author_id";
 
-const rules = [{ value: "f1" }];
+const rules = [{ value: "F1 lang:en" }];
 
 // STREAM RULE FUNCTIONS
 
